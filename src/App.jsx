78440d27c2a0a1914c15dada730efa8d72d99dcd
viplavg <viplavg1999@gmail.com>
2025-03-13 00:31:47 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 function App() {
-  return <h1>Welcome to EverEdge</h1>;
+  return (
+    <>
+      <h1>Welcome to EverEdge</h1>
+      <h1>Welcome to EverEdge</h1>
+      <h1>Welcome to EverEdge</h1>
+      <h1>Welcome to EverEdge</h1>
+    </>
+  );
 }
 
 export default App;
